31e1c6631a8ae00e310cc5f55216f04ffea0eec8
Sergey Firsov <fearthunder@yandex.ru>
2020-03-14 18:09:21 +0300

    initial commit

---
+++ Source/BacterialSimulation/BacterialSimulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BacterialSimulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BACTERIALSIMULATION_API ABacterialSimulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
