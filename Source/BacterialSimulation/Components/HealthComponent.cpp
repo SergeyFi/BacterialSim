2ea0b4fcf5904c50f26bed27292e3994136b1cd0
Sergey Firsov <fearthunder@yandex.ru>
2020-03-14 18:27:23 +0300

    Add health component class

---
+++ Source/BacterialSimulation/Components/HealthComponent.cpp
@@ -0,0 +1,20 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
