f791103c13236e7174fb04ed4acc9e54cdabb250
Sergey Firsov <fearthunder@yandex.ru>
2020-03-15 23:33:25 +0300

    Add gene

---
+++ Source/BacterialSimulation/Objects/Genes/Gene.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Gene.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGene : public UObject
+{
+	GENERATED_BODY()
+	
+};
