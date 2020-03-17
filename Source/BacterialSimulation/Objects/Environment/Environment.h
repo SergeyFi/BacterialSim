933792f1318abe15afd7164b847f2643a5043b95
Sergey Firsov <fearthunder@yandex.ru>
2020-03-17 21:39:35 +0300

    Add environment class

---
+++ Source/BacterialSimulation/Objects/Environment/Environment.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Environment.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UEnvironment : public UObject
+{
+	GENERATED_BODY()
+	
+};
