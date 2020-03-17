7ee769d5cb96bb029f97d6d618fd27f2ecff92de
Sergey Firsov <fearthunder@yandex.ru>
2020-03-17 12:35:59 +0300

    Add gene of binary division

---
+++ Source/BacterialSimulation/Objects/Genes/GeneBinaryDivision.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Objects/Genes/Gene.h"
+#include "GeneBinaryDivision.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneBinaryDivision : public UGene
+{
+	GENERATED_BODY()
+	
+};
