2cdf0af2fc5817d546783e43252c5e67bbb5a742
Sergey Firsov <fearthunder@yandex.ru>
2020-03-17 12:49:58 +0300

    Add gene interface

---
+++ Source/BacterialSimulation/Interfaces/GeneInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "GeneInterface.h"
+
+// Add default functionality here for any IGeneInterface functions that are not pure virtual.
---
+++ Source/BacterialSimulation/Interfaces/GeneInterface.h
@@ -0,0 +1,28 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GeneInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UGeneInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class BACTERIALSIMULATION_API IGeneInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	class UHealthComponent* GetHealthComponent();
+
+	class UEnergyComponent* GetEnergyComponent();
+
+
+};
