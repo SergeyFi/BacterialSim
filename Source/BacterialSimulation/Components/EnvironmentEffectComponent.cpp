36fe0f6b4997330071141fb9d5521b1327f6a695
Sergey Firsov <fearthunder@yandex.ru>
2020-03-18 23:14:00 +0300

    Add environment effect component

---
+++ Source/BacterialSimulation/Components/EnvironmentEffectComponent.cpp
@@ -0,0 +1,19 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "EnvironmentEffectComponent.h"
+
+// Sets default values for this component's properties
+UEnvironmentEffectComponent::UEnvironmentEffectComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+
+// Called when the game starts
+void UEnvironmentEffectComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
