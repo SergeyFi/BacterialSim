45b0583588ee306b70fac649175ff7098b2b6015
Sergey Firsov <fearthunder@yandex.ru>
2020-03-18 15:15:46 +0300

    Add get environment to protected

--- Source/BacterialSimulation/Interfaces/EnvironmentInterface.h
+++ Source/BacterialSimulation/Interfaces/EnvironmentInterface.h
@@ -19,7 +19,7 @@ class BACTERIALSIMULATION_API IEnvironmentInterface
 	GENERATED_BODY()
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
-public:
+protected:
 
 	virtual class UEnvironment* GetEnvironment();
 };
