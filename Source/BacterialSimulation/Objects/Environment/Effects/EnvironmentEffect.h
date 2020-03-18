63758e536615d60e1497516b20f8ecee4389aafb
Sergey Firsov <fearthunder@yandex.ru>
2020-03-18 22:50:24 +0300

    Add environment effect

---
+++ Source/BacterialSimulation/Objects/Environment/Effects/EnvironmentEffect.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "EnvironmentEffect.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UEnvironmentEffect : public UObject
+{
+	GENERATED_BODY()
+	
+};
