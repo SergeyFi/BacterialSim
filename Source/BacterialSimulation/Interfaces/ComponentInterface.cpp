ddd244b92af1b78fa20483e24c6358380b5001ae
Sergey Firsov <fearthunder@yandex.ru>
2020-03-18 23:41:30 +0300

    Add component interface

---
+++ Source/BacterialSimulation/Interfaces/ComponentInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "ComponentInterface.h"
+
+// Add default functionality here for any IComponentInterface functions that are not pure virtual.
---
+++ Source/BacterialSimulation/Interfaces/ComponentInterface.h
@@ -0,0 +1,23 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ComponentInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UComponentInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+
+class BACTERIALSIMULATION_API IComponentInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
