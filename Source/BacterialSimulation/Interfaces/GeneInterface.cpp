b9dd1709c83434f86114cf4dd26f0e7361de218b
Sergey Firsov <fearthunder@yandex.ru>
2020-03-19 14:52:45 +0300

    Remove old interfaces

--- Source/BacterialSimulation/Interfaces/GeneInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2020 Sergey Firsov. All Rights Reserved.
-
-
-#include "GeneInterface.h"
-
-// Add default functionality here for any IGeneInterface functions that are not pure virtual.
