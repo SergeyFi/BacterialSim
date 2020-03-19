ebe56240f1003ea88d67cdb8947fd0ac20ecf7c4
Sergey Firsov <fearthunder@yandex.ru>
2020-03-19 21:25:42 +0300

    Add size component class

---
+++ Source/BacterialSimulation/Components/SizeComponent.cpp
@@ -0,0 +1,20 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "SizeComponent.h"
+
+// Sets default values for this component's properties
+USizeComponent::USizeComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+
+}
+
+
+// Called when the game starts
+void USizeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
---
+++ Source/BacterialSimulation/Components/SizeComponent.h
@@ -0,0 +1,22 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SizeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BACTERIALSIMULATION_API USizeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USizeComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;		
+};
