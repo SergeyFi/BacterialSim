df77102c0c3dcdcb438427536fa5d35fa2d8c832
Sergey Firsov <fearthunder@yandex.ru>
2020-03-20 20:18:55 +0300

    Setup binary fision

--- Source/BacterialSimulation/Genes/GeneBinaryFission.cpp
+++ Source/BacterialSimulation/Genes/GeneBinaryFission.cpp
@@ -3,3 +3,128 @@
 
 #include "GeneBinaryFission.h"
 
+#include "Engine/World.h"
+
+#include "BacterialSimulation/Interfaces/ComponentInterface.h"
+
+#include "BacterialSimulation/Components/EnergyComponent.h"
+#include "BacterialSimulation/Components/HealthComponent.h"
+
+
+UGeneBinaryFission::UGeneBinaryFission() 
+{
+    MinimumHealthToFission = 90.0f;
+    MinimumEnergyToFission = 90.0f;
+    EnergyWasteOnFission = 50.0f;
+    ConditionsCheckPeriod = 3.0f;
+}
+
+void UGeneBinaryFission::StartConditionCheck() 
+{
+    UWorld* World = GetWorld();
+    
+    if (World)
+    {
+        World->GetTimerManager().SetTimer(TimerConditionChecker, this, &UGeneBinaryFission::ConditionCheck, ConditionsCheckPeriod, true);
+    }
+}
+
+void UGeneBinaryFission::ConditionCheck() 
+{
+    if (OwnerComponents)
+    {
+        float EnergyCurrent = OwnerComponents->GetEnergyComponent()->GetEnergyCurrent();
+        float HealthCurrent = OwnerComponents->GetHealthComponent()->GetHealthCurrent();
+
+        if (EnergyCurrent >= MinimumEnergyToFission && HealthCurrent >= MinimumHealthToFission)
+        {
+            BinaryFission();
+        }
+    }
+}
+
+void UGeneBinaryFission::StopConditionCheck() 
+{
+    UWorld* World = GetWorld();
+    
+    if (World)
+    {
+        World->GetTimerManager().ClearTimer(TimerConditionChecker);
+    }
+}
+
+void UGeneBinaryFission::ActivateGene() 
+{
+    if (Owner)
+    {
+        bIsActive = true;
+
+        StartConditionCheck();
+    }
+}
+
+void UGeneBinaryFission::DeactivateGene() 
+{
+    bIsActive = false;
+
+    StopConditionCheck();
+}
+
+void UGeneBinaryFission::BinaryFission() 
+{
+    float CheckStep = 10.0f;
+    FVector Origin;
+    FVector BoxExtent;
+    FVector SpawnLocation;
+
+    Owner->GetActorBounds(true, Origin, BoxExtent);
+
+    BoxExtent.X += 10.0f;
+    BoxExtent.Y += 10.0f;
+
+    if (BoxExtent.X > BoxExtent.Y) BoxExtent.Y = BoxExtent.X;
+
+    for (float Angle = 0.0f ; Angle < 360.0f; Angle += CheckStep)
+    {
+        float X = BoxExtent.Y * FMath::Cos(Angle);
+        float Y = BoxExtent.Y * FMath::Sin(Angle);
+
+        SpawnLocation.X = X + Origin.X;
+        SpawnLocation.Y = Y + Origin.Y;
+
+        UWorld* World = GetWorld();
+
+        if (World)
+        {
+            FHitResult HitResult;
+
+            FCollisionShape CollisionSphere = FCollisionShape::MakeSphere(BoxExtent.Y*2.0f);
+
+            bool IsHit = World->SweepSingleByChannel(HitResult, SpawnLocation, 
+            FVector(X, Y, 1.0f), FQuat::Identity, ECC_WorldStatic, CollisionSphere);
+
+            if (!IsHit)
+            {
+                SpawnOwnerCopy(SpawnLocation);
+                break;
+            }
+        }
+    }
+
+}
+
+void UGeneBinaryFission::SpawnOwnerCopy(FVector SpawnLocation) 
+{
+    UWorld* World = GetWorld();
+    
+    if (World)
+    {
+        FActorSpawnParameters SpawnParams;
+        SpawnParams.Template = Owner;
+    
+        FTransform SpawnTransform;
+        SpawnTransform.SetLocation(SpawnLocation);
+    
+        World->SpawnActor<AActor>(Owner->GetClass(), SpawnTransform, SpawnParams);
+    }
+}
--- Source/BacterialSimulation/Genes/GeneBinaryFission.h
+++ Source/BacterialSimulation/Genes/GeneBinaryFission.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "BacterialSimulation/Genes/Gene.h"
+
+#include "Engine/EngineTypes.h"
+
 #include "GeneBinaryFission.generated.h"
 
 
@@ -11,5 +14,38 @@ UCLASS()
 class BACTERIALSIMULATION_API UGeneBinaryFission : public UGene
 {
 	GENERATED_BODY()
-	
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gene")
+	float MinimumHealthToFission;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gene")
+	float MinimumEnergyToFission;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gene")
+	float EnergyWasteOnFission;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gene")
+	float ConditionsCheckPeriod;
+
+	FTimerHandle TimerConditionChecker;
+
+	void StartConditionCheck();
+
+	void ConditionCheck();
+
+	void StopConditionCheck();
+
+	virtual void ActivateGene() override;
+
+	virtual void DeactivateGene() override;
+
+	void BinaryFission();
+
+	void SpawnOwnerCopy(FVector SpawnLocation);
+
+public:
+
+	UGeneBinaryFission();
 };
