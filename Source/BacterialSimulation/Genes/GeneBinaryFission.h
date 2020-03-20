76ece459eea91612b5e86ed7a9e5d85609495d11
Sergey Firsov <fearthunder@yandex.ru>
2020-03-20 14:47:11 +0300

    Add gene of binary fission

---
+++ Source/BacterialSimulation/Genes/GeneBinaryFission.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Gene.h"
+#include "GeneBinaryFission.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneBinaryFission : public UGene
+{
+	GENERATED_BODY()
+	
+};
