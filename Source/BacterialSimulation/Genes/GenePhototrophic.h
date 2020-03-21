d33d32ef31017a47ead9a4287a9e9b7921eda694
Sergey Firsov <fearthunder@yandex.ru>
2020-03-21 18:36:10 +0300

    Add base phototrophic gene

---
+++ Source/BacterialSimulation/Genes/GenePhototrophic.h
@@ -0,0 +1,14 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Gene.h"
+#include "GenePhototrophic.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGenePhototrophic : public UGene
+{
+	GENERATED_BODY()
+};
