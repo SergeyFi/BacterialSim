666893f3b9c991bee57c04a510d9d72087fbdb0e
Sergey Firsov <fearthunder@yandex.ru>
2020-03-21 18:30:05 +0300

    Add base light effect

---
+++ Source/BacterialSimulation/Environment/Effects/Light/LightEffect.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Environment/Effects/EnvironmentEffect.h"
+#include "LightEffect.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API ULightEffect : public UEnvironmentEffect
+{
+	GENERATED_BODY()
+	
+};
