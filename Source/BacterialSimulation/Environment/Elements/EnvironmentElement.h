3809334ab43997e82c1f3fe9f7221dbbd9a9b5c3
Sergey Firsov <fearthunder@yandex.ru>
2020-03-23 12:07:18 +0300

    Add environment element

---
+++ Source/BacterialSimulation/Environment/Elements/EnvironmentElement.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "EnvironmentElement.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UEnvironmentElement : public UObject
+{
+	GENERATED_BODY()
+	
+};
