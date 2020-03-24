1d38f08d2d4cd6b5b20a060ef39485bc9a83274f
Sergey Firsov <fearthunder@yandex.ru>
2020-03-24 17:03:42 +0300

    Add gene: size gene

---
+++ Source/BacterialSimulation/Genes/GeneSize.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Gene.h"
+#include "GeneSize.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneSize : public UGene
+{
+	GENERATED_BODY()
+	
+};
