49f15d2ab598bbe7a6109152848ba5c8805a5276
Sergey Firsov <fearthunder@yandex.ru>
2020-03-24 23:50:07 +0300

    Remove old interfaces

--- Source/BacterialSimulation/Interfaces/ComponentInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2020 Sergey Firsov. All Rights Reserved.
-
-
-#include "ComponentInterface.h"
-
-// Add default functionality here for any IComponentInterface functions that are not pure virtual.
