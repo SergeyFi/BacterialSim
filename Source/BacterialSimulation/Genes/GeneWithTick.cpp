ec7228aaf903dcc01c9a8cc35ad7d1bd26f3f0ce
Sergey Firsov <fearthunder@yandex.ru>
2020-03-26 14:03:45 +0300

    Add gene with tick

---
+++ Source/BacterialSimulation/Genes/GeneWithTick.cpp
@@ -0,0 +1,36 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "GeneWithTick.h"
+
+#include "Engine/Engine.h"
+
+UGeneWithTick::UGeneWithTick() 
+{
+
+}
+
+void UGeneWithTick::Tick(float DeltaTime) 
+{
+    
+}
+
+bool UGeneWithTick::IsTickable() const 
+{
+    return true;
+}
+
+bool UGeneWithTick::IsTickableInEditor() const 
+{
+    return false;
+}
+
+bool UGeneWithTick::IsTickableWhenPaused() const 
+{
+    return false;
+}
+
+TStatId UGeneWithTick::GetStatId() const 
+{
+    return TStatId();
+}
---
+++ Source/BacterialSimulation/Genes/GeneWithTick.h
@@ -0,0 +1,38 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Gene.h"
+
+#include "Tickable.h"
+
+#include "GeneWithTick.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneWithTick : public UGene, public FTickableGameObject
+{
+	GENERATED_BODY()
+
+private:
+
+	bool bIsCreateOnRunning;
+
+protected:
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual bool IsTickable() const override;
+
+	virtual bool IsTickableInEditor() const override;
+
+	virtual bool IsTickableWhenPaused() const override;
+
+	virtual TStatId GetStatId() const override;
+
+public:
+
+	UGeneWithTick();
+	
+};
