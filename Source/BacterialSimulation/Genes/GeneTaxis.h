668635fc7d8e0fffdfd150b066293bc7756780da
Sergey Firsov <fearthunder@yandex.ru>
2020-03-27 23:10:28 +0300

    Add base gene of taxis

---
+++ Source/BacterialSimulation/Genes/GeneTaxis.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Gene.h"
+#include "GeneTaxis.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneTaxis : public UGene
+{
+	GENERATED_BODY()
+	
+};
