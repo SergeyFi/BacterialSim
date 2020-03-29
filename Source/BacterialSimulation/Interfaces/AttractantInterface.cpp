a8c23c262c849105f5fd1fd48a5f4885256ae1c1
Sergey Firsov <fearthunder@yandex.ru>
2020-03-29 23:21:07 +0300

    Add attractant interface

---
+++ Source/BacterialSimulation/Interfaces/AttractantInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "AttractantInterface.h"
+
+// Add default functionality here for any IAttractantInterface functions that are not pure virtual.
---
+++ Source/BacterialSimulation/Interfaces/AttractantInterface.h
@@ -0,0 +1,22 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "AttractantInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UAttractantInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class BACTERIALSIMULATION_API IAttractantInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
