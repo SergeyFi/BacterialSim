3b155bbb8338f1c4f44d4cd43a89ca2cd46140fc
Sergey Firsov <fearthunder@yandex.ru>
2020-04-02 16:31:26 +0300

    Add gene of lisis

---
+++ Source/BacterialSimulation/Genes/GeneLisis.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Gene.h"
+#include "GeneLisis.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneLisis : public UGene
+{
+	GENERATED_BODY()
+	
+};
