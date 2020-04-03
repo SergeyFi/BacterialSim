7a24efe44a3dbe02a7d9781731c8152ab600ad67
Sergey Firsov <fearthunder@yandex.ru>
2020-04-03 15:03:36 +0300

    Add molecule of protein class

---
+++ Source/BacterialSimulation/Molecules/MoleculeProtein.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Molecules/Molecule.h"
+#include "MoleculeProtein.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API AMoleculeProtein : public AMolecule
+{
+	GENERATED_BODY()
+	
+};
