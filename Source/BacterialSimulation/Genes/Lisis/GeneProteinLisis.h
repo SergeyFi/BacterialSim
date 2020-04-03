24e346d35dbb6392309a568d389d2636aa2a419e
Sergey Firsov <fearthunder@yandex.ru>
2020-04-03 14:57:36 +0300

    Add gene of protetin lisis and remove gene of lisis to other directory

---
+++ Source/BacterialSimulation/Genes/Lisis/GeneProteinLisis.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Genes/Lisis/GeneLisis.h"
+#include "GeneProteinLisis.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UGeneProteinLisis : public UGeneLisis
+{
+	GENERATED_BODY()
+	
+};
