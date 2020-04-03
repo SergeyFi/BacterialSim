3f9e04da7fb8874bd7b2745329b3dad451ab2672
Sergey Firsov <fearthunder@yandex.ru>
2020-04-03 22:02:36 +0300

    Add nutrient component interface

---
+++ Source/BacterialSimulation/Interfaces/NutrientComponentInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+
+#include "NutrientComponentInterface.h"
+
+// Add default functionality here for any INutrientComponentInterface functions that are not pure virtual.
