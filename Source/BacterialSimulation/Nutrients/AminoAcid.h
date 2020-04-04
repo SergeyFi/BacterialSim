ca46a741606b11c8bdd4e01970fb063ac1c10aaa
Sergey Firsov <fearthunder@yandex.ru>
2020-04-04 16:26:32 +0300

    Add amino acid nutrient

---
+++ Source/BacterialSimulation/Nutrients/AminoAcid.h
@@ -0,0 +1,15 @@
+// Copyright 2020 Sergey Firsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BacterialSimulation/Nutrients/Nutrient.h"
+#include "AminoAcid.generated.h"
+
+
+UCLASS()
+class BACTERIALSIMULATION_API UAminoAcid : public UNutrient
+{
+	GENERATED_BODY()
+	
+};
